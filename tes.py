f1e45b14920936dfac44969abd1b00fd9c99fcdd
Imam Hatris Ekaputra <95421498+Imamh28@users.noreply.github.com>
2024-06-06 18:25:27 +0700

    Update tes.py

--- tes.py
+++ tes.py
@@ -32,7 +32,7 @@ popular_titles = {category: get_popular_title(df_buku, category) for category in
 def get_genre_recommendations(selected_genres, cosine_sim=cosine_sim, df=df_buku):
     indices = df.index[df['categories'].isin(selected_genres)].tolist()
     if not indices:
-        return pd.DataFrame()
+        return pd.DataFrame(), []
 
     avg_sim_scores = np.mean(cosine_sim[indices], axis=0)
     sim_scores = list(enumerate(avg_sim_scores))
@@ -40,19 +40,18 @@ def get_genre_recommendations(selected_genres, cosine_sim=cosine_sim, df=df_buku
     sim_scores = sim_scores[1:6]
     book_indices = [i[0] for i in sim_scores]
     recommended_books = df.iloc[book_indices][['title', 'authors', 'average_rating']]
-    return recommended_books
+    return recommended_books, sim_scores
 
 # Function to get genre recommendations with user preferences
 def get_genre_recommendations_with_preferences(selected_genres, reading_type, popularity, rating_influence, selected_book, cosine_sim=cosine_sim, df=df_buku):
     indices = df.index[df['categories'].isin(selected_genres)].tolist()
     if not indices:
-        return pd.DataFrame()
+        return pd.DataFrame(), []
 
     avg_sim_scores = np.mean(cosine_sim[indices], axis=0)
     sim_scores = list(enumerate(avg_sim_scores))
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
     sim_scores = sim_scores[1:6]
-    print(sim_scores)
     book_indices = [i[0] for i in sim_scores]
     recommended_books = df.iloc[book_indices][['title', 'authors', 'average_rating', 'num_pages']]
 
@@ -66,10 +65,10 @@ def get_genre_recommendations_with_preferences(selected_genres, reading_type, po
         recommended_books = pd.concat([recommended_books, popular_books_df])
 
     if rating_influence == 'Ya' and selected_book:
-        similar_books = get_genre_recommendations([selected_book], cosine_sim=cosine_sim, df=df)
+        similar_books, _ = get_genre_recommendations([selected_book], cosine_sim=cosine_sim, df=df)
         recommended_books = pd.concat([recommended_books, similar_books])
 
-    return recommended_books.drop_duplicates()
+    return recommended_books.drop_duplicates(), sim_scores
 
 # Function to get user feedback
 def get_user_feedback():
@@ -117,9 +116,8 @@ def main():
                             confirm = st.button("Konfirmasi Pilihan")
                             if confirm:
                                 st.write("Berikut beberapa buku yang mungkin menarik bagi Anda:")
-                                recommendations = get_genre_recommendations(selected_genres)
-
-                                recommended_books = get_genre_recommendations_with_preferences(selected_genres, reading_type, popularity, rating_influence, selected_book)
+                                recommendations, genre_sim_scores = get_genre_recommendations(selected_genres)
+                                recommended_books, overall_sim_scores = get_genre_recommendations_with_preferences(selected_genres, reading_type, popularity, rating_influence, selected_book)
 
                                 if recommendations.empty:
                                     st.write("Maaf, kami tidak dapat menemukan rekomendasi berdasarkan genre yang dipilih.")
@@ -127,16 +125,20 @@ def main():
                                     st.write("Rekomendasi berdasarkan genre:")
                                     st.dataframe(recommendations)
 
+                                    st.write("Similarity Scores untuk rekomendasi berdasarkan genre:")
+                                    st.write(genre_sim_scores)
+
                                     st.write("Rekomendasi berdasarkan keseluruhan:")
                                     st.dataframe(recommended_books[['title', 'authors', 'average_rating']], use_container_width=True)
 
-                                    
-                                    
-                                    feedback = get_user_feedback()
-                                    if feedback:
-                                        st.write("Terima kasih atas umpan balik Anda!")
-                                    else:
-                                        st.write("Kami menunggu masukan dari Anda! Silakan isi feedback Anda di atas untuk membantu kami memberikan rekomendasi yang lebih baik.")
+                                    st.write("Similarity Scores untuk rekomendasi berdasarkan keseluruhan:")
+                                    st.write(overall_sim_scores)
+
+                                feedback = get_user_feedback()
+                                if feedback:
+                                    st.write("Terima kasih atas umpan balik Anda!")
+                                else:
+                                    st.write("Kami menunggu masukan dari Anda! Silakan isi feedback Anda di atas untuk membantu kami memberikan rekomendasi yang lebih baik.")
 
 if __name__ == "__main__":
     main()
