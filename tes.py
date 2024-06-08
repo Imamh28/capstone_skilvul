d041e950d24907977a43a25eb74424133abc6ae4
Imam Hatris Ekaputra <95421498+Imamh28@users.noreply.github.com>
2024-06-08 18:29:53 +0700

    Update tes.py

--- tes.py
+++ tes.py
@@ -6,12 +6,6 @@ from sklearn.metrics.pairwise import cosine_similarity
 from ibm_watson import NaturalLanguageUnderstandingV1
 from ibm_watson.natural_language_understanding_v1 import Features, SentimentOptions
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-import ssl
-
-# Tidak memeriksa sertifikat SSL
-ssl_context = ssl.create_default_context()
-ssl_context.check_hostname = False
-ssl_context.verify_mode = ssl.CERT_NONE
 
 # Initialize IBM NLU
 api_key = 'sSXHNVTN-iWR7PhHP3t2lLFr2k7oYNn4YXYXBM99wesI'
@@ -24,8 +18,7 @@ nlu = NaturalLanguageUnderstandingV1(
     disable_ssl_verification=True  # Tambahkan opsi ini
 )
 
-# Set service URL
-nlu.set_service_url(service_url, ssl_context=ssl_context)
+nlu.set_service_url(service_url)
 
 # Function to analyze feedback using IBM NLU
 def analyze_feedback(feedback_text):
@@ -39,7 +32,6 @@ def analyze_feedback(feedback_text):
     except Exception as e:
         st.error(f"Error analyzing feedback: {e}")
         return None
-
         
 # Load dataset
 df_buku = pd.read_csv('books.csv')
